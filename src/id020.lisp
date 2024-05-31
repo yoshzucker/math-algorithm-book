990d203aed9de3e27dbaed35d86bfdf8e5b648d0
yoshzucker <yoshzucker@gmail.com>
2024-05-31 16:23:55 +0900

    refactor: id020

--- src/id020.lisp
+++ src/id020.lisp
@@ -1,10 +1,14 @@
 (let* ((n (read))
        (al (loop repeat n collect (read))))
-  (loop for b below (expt 2 n)
-        count (and (= (logcount b) 5)
-                   (loop for i below n
-                         when (logbitp i b)
-                           sum (nth i al) into s
-                         finally (return (= s 1000))))
-          into c
+  (loop for i below n with c = 0
+        do (loop for j from (1+ i) below n
+                 do (loop for k from (1+ j) below n
+                          do (loop for l from (1+ k) below n
+                                   do (loop for m from (1+ l) below n
+                                            when (= 1000 (+ (nth i al)
+                                                            (nth j al)
+                                                            (nth k al)
+                                                            (nth l al)
+                                                            (nth m al)))
+                                              do (incf c)))))
         finally (print c)))
