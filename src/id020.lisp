722737ae1f6901fc5dc1f5da31346e0991b1ad29
yoshzucker <yoshzucker@gmail.com>
2024-05-31 16:35:00 +0900

    perf: change list to array

--- src/id020.lisp
+++ src/id020.lisp
@@ -1,14 +1,15 @@
 (let* ((n (read))
-       (al (loop repeat n collect (read))))
+       (aa (make-array n :fill-pointer 0)))
+  (loop repeat n do (vector-push (read) aa))
   (loop for i below n with c = 0
         do (loop for j from (1+ i) below n
                  do (loop for k from (1+ j) below n
                           do (loop for l from (1+ k) below n
                                    do (loop for m from (1+ l) below n
-                                            when (= 1000 (+ (nth i al)
-                                                            (nth j al)
-                                                            (nth k al)
-                                                            (nth l al)
-                                                            (nth m al)))
+                                            when (= 1000 (+ (elt aa i)
+                                                            (elt aa j)
+                                                            (elt aa k)
+                                                            (elt aa l)
+                                                            (elt aa m)))
                                               do (incf c)))))
         finally (print c)))
