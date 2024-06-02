07f36fb5e9b3baf2410822c15ef2e9e53d731921
yoshzucker <yoshzucker@gmail.com>
2024-06-02 21:49:14 +0900

    refactor: id027

--- src/id027.lisp
+++ src/id027.lisp
@@ -1,34 +1,18 @@
-(defparameter *n* (read))
-(defparameter *a* (make-array (1+ *n*) :fill-pointer 1))
-(defparameter *c* (make-array (1+ *n*) :initial-element 0))
-(loop repeat *n* do (vector-push (read) *a*))
+(defun merge-sort (lst)
+  (if (= (length lst) 1) lst
+      (let ((lstl (merge-sort (subseq lst 0 (floor (length lst) 2))))
+            (lstr (merge-sort (subseq lst (floor (length lst) 2)))))
+        (loop with lstc while (or lstl lstr)
+              do (cond ((null lstl)
+                        (push (pop lstr) lstc))
+                       ((null lstr)
+                        (push (pop lstl) lstc))
+                       ((< (first lstl) (first lstr))
+                        (push (pop lstl) lstc))
+                       (t
+                        (push (pop lstr) lstc)))
+              finally (return (nreverse lstc))))))
 
-(defun my-merge-sort (l r)
-  (if (= (- r l) 1) nil
-      (let ((m (floor (+ l r) 2)))
-        (my-merge-sort l m)
-        (my-merge-sort m r)
-        
-        (let ((c1 l)
-              (c2 m)
-              (cnt 0))
-          (loop while (or (/= c1 m) (/= c2 r))
-                do (cond ((= c1 m)
-                          (setf (elt *c* cnt) (elt *a* c2))
-                          (incf c2))
-                         ((= c2 r)
-                          (setf (elt *c* cnt) (elt *a* c1))
-                          (incf c1))
-                         (t
-                          (cond ((< (elt *a* c2) (elt *a* c1))
-                                 (setf (elt *c* cnt) (elt *a* c2))
-                                 (incf c2))
-                                (t
-                                 (setf (elt *c* cnt) (elt *a* c1))
-                                 (incf c1)))))
-                   (incf cnt))
-          (loop for i below cnt
-                do (setf (elt *a* (+ l i)) (elt *c* i)))))))
-
-(my-merge-sort 1 (1+ *n*))
-(loop for i from 1 to *n* do (format t "~a " (elt *a* i)))
+(let* ((n (read))
+       (ai (loop repeat n collect (read))))
+  (format t "~{~a ~}" (merge-sort ai)))
