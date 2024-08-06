e3110c797ed38ecd88103f62ba532b3269a85864
yoshzucker <yoshzucker@gmail.com>
2024-08-06 21:41:31 +0900

    feat: id032

--- src/id032/id032-o(logn)2.lisp
+++ src/id032/id032-o(logn)2.lisp
@@ -1,13 +1,13 @@
 (let* ((n (read))
        (x (read))
        (an (sort (loop repeat n collect (read)) #'<)))
-  (loop while (<= left right)
-        with left = 1 and right = n
-        do (let ((mid (floor (+ left right) 2)))
-             (cond ((= x (elt an mid))
-                    (return (format t "Yes")))
-                   ((< x (elt an mid))
-                    (setf right (1- mid)))
-                   (t
-                    (setf left (1+ mid)))))
-        finally (format t "No")))
+  (loop with l = 0 and r = (1- n)
+        while (<= l r)
+        for m = (floor (+ l r) 2)
+        do (cond ((< x (elt an m))
+                  (setf r (1- m)))
+                 ((< (elt an m) x)
+                  (setf l (1+ m)))
+                 (t
+                  (return (princ "Yes"))))
+        finally (princ "No")))
