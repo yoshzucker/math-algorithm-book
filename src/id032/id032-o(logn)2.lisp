ab51e43dc32f8bd7d2dbf94f9507e3b37623ed96
yoshzucker <yoshzucker@gmail.com>
2024-08-07 16:42:11 +0900

    refactor: id032

--- src/id032/id032-o(logn)2.lisp
+++ src/id032/id032-o(logn)2.lisp
@@ -1,13 +1,17 @@
+(defun binary-search (sequence target fn)
+  (loop with l = 0 and r = (1- (length sequence))
+        for m = (floor (+ l r) 2)
+        always (<= l r)
+        do (let ((mvalue (funcall fn m)))
+             (cond ((< target mvalue)
+                    (setf r (1- m)))
+                   ((< mvalue target)
+                    (setf l (1+ m)))
+                   (t
+                    (return t))))))
+
 (let* ((n (read))
        (x (read))
        (an (sort (loop repeat n collect (read)) #'<)))
-  (loop with l = 0 and r = (1- n)
-        while (<= l r)
-        for m = (floor (+ l r) 2)
-        do (cond ((< x (elt an m))
-                  (setf r (1- m)))
-                 ((< (elt an m) x)
-                  (setf l (1+ m)))
-                 (t
-                  (return (princ "Yes"))))
-        finally (princ "No")))
+  (format t "~:[No~;Yes~]"
+          (binary-search an x #'(lambda (x) (elt an x)))))
